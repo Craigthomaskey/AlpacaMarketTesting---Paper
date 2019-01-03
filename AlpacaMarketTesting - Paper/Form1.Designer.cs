896f7718827553fb96c265c73d650274cedfd85b
Craigthomaskey <craigthomaskey@gmail.com>
2019-01-03 15:07:52 -0600

    Started connection testing.

--- AlpacaMarketTesting - Paper/Form1.Designer.cs
+++ AlpacaMarketTesting - Paper/Form1.Designer.cs
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GetTimeBttn = new System.Windows.Forms.Button();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.AccntBttn = new System.Windows.Forms.Button();
+            this.AccntLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GetTimeBttn
+            // 
+            this.GetTimeBttn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GetTimeBttn.Location = new System.Drawing.Point(12, 59);
+            this.GetTimeBttn.Name = "GetTimeBttn";
+            this.GetTimeBttn.Size = new System.Drawing.Size(75, 23);
+            this.GetTimeBttn.TabIndex = 0;
+            this.GetTimeBttn.Text = "Get Time";
+            this.GetTimeBttn.UseVisualStyleBackColor = true;
+            this.GetTimeBttn.Click += new System.EventHandler(this.GetTimeBttn_Click);
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TimeLabel.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLabel.Location = new System.Drawing.Point(12, 9);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(535, 47);
+            this.TimeLabel.TabIndex = 1;
+            this.TimeLabel.Text = "Time";
+            this.TimeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AccntBttn
+            // 
+            this.AccntBttn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AccntBttn.Location = new System.Drawing.Point(457, 59);
+            this.AccntBttn.Name = "AccntBttn";
+            this.AccntBttn.Size = new System.Drawing.Size(90, 23);
+            this.AccntBttn.TabIndex = 0;
+            this.AccntBttn.Text = "Account Info";
+            this.AccntBttn.UseVisualStyleBackColor = true;
+            this.AccntBttn.Click += new System.EventHandler(this.AccntBttn_Click);
+            // 
+            // AccntLabel
+            // 
+            this.AccntLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AccntLabel.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AccntLabel.Location = new System.Drawing.Point(12, 85);
+            this.AccntLabel.Name = "AccntLabel";
+            this.AccntLabel.Size = new System.Drawing.Size(535, 47);
+            this.AccntLabel.TabIndex = 1;
+            this.AccntLabel.Text = "Info";
+            this.AccntLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(558, 147);
+            this.Controls.Add(this.AccntLabel);
+            this.Controls.Add(this.AccntBttn);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.GetTimeBttn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button GetTimeBttn;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Button AccntBttn;
+        private System.Windows.Forms.Label AccntLabel;
     }
 }
 
--- AlpacaMarketTesting - Paper/Form1.cs
+++ AlpacaMarketTesting - Paper/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace AlpacaMarketTesting___Paper
 {
     public partial class Form1 : Form
@@ -15,6 +16,40 @@ namespace AlpacaMarketTesting___Paper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Alpaca.Markets.RestClient Client = new Alpaca.Markets.RestClient("PKKRRSRMHXLDU4HF9VCC", "Tl27vcracryyxlL0DFExvJcgTarl/UjaLqmlSSFt", "https://paper-api.alpaca.markets");
+        async Task ClockTestAsync()
+        {           
+
+            var Clock = await Client.GetClockAsync();
+
+            if(Clock != null)
+            {
+                TimeLabel.Text = string.Format("TimeStamp : {0}, NextOpen : {1}, NextClose : {2}", Clock.Timestamp, Clock.NextOpen, Clock.NextClose);
+            }
+
+        }
+
+        private void GetTimeBttn_Click(object sender, EventArgs e)
+        {
+            ClockTestAsync();
+        }
+
+        private void AccntBttn_Click(object sender, EventArgs e)
+        {
+            AccntInfoTestAsync();
+        }
+
+        async Task AccntInfoTestAsync()
+        {
+
+            var AccountInfo = await Client.GetAccountAsync();
+
+            if (AccountInfo != null)
+            {
+                AccntLabel.Text = AccountInfo.Status + " - $" + AccountInfo.PortfolioValue;
+            }
         }
     }
 }
